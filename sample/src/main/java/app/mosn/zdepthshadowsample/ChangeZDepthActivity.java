c5ff9c74b515514c180109e2dc10e55290225185
ShogoMizumoto <syogo330@gmail.com>
2014-12-01 13:03:52 +0900

    fixed add CircleZDepthShadow to ChangeZDepth sample

--- sample/src/main/java/app/mosn/zdepthshadowsample/ChangeZDepthActivity.java
+++ sample/src/main/java/app/mosn/zdepthshadowsample/ChangeZDepthActivity.java
@@ -11,8 +11,11 @@ import butterknife.OnClick;
 
 public class ChangeZDepthActivity extends ActionBarActivity {
 
-    @InjectView(R.id.zDepthShadowLayout)
-    ZDepthShadowLayout mZDepthShadowLayout;
+    @InjectView(R.id.zDepthShadowLayout_rect)
+    ZDepthShadowLayout mZDepthShadowLayoutRect;
+
+    @InjectView(R.id.zDepthShadowLayout_oval)
+    ZDepthShadowLayout mZDepthShadowLayoutOval;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,31 +26,37 @@ public class ChangeZDepthActivity extends ActionBarActivity {
 
     @OnClick(R.id.button_zDepth_0)
     protected void onClickZDepth0() {
-        mZDepthShadowLayout.changeZDepth(ZDepth.Depth0);
+        mZDepthShadowLayoutRect.changeZDepth(ZDepth.Depth0);
+        mZDepthShadowLayoutOval.changeZDepth(ZDepth.Depth0);
     }
 
     @OnClick(R.id.button_zDepth_1)
     protected void onClickZDepth1() {
-        mZDepthShadowLayout.changeZDepth(ZDepth.Depth1);
+        mZDepthShadowLayoutRect.changeZDepth(ZDepth.Depth1);
+        mZDepthShadowLayoutOval.changeZDepth(ZDepth.Depth1);
     }
 
     @OnClick(R.id.button_zDepth_2)
     protected void onClickZDepth2() {
-        mZDepthShadowLayout.changeZDepth(ZDepth.Depth2);
+        mZDepthShadowLayoutRect.changeZDepth(ZDepth.Depth2);
+        mZDepthShadowLayoutOval.changeZDepth(ZDepth.Depth2);
     }
 
     @OnClick(R.id.button_zDepth_3)
     protected void onClickZDepth3() {
-        mZDepthShadowLayout.changeZDepth(ZDepth.Depth3);
+        mZDepthShadowLayoutRect.changeZDepth(ZDepth.Depth3);
+        mZDepthShadowLayoutOval.changeZDepth(ZDepth.Depth3);
     }
 
     @OnClick(R.id.button_zDepth_4)
     protected void onClickZDepth4() {
-        mZDepthShadowLayout.changeZDepth(ZDepth.Depth4);
+        mZDepthShadowLayoutRect.changeZDepth(ZDepth.Depth4);
+        mZDepthShadowLayoutOval.changeZDepth(ZDepth.Depth4);
     }
 
     @OnClick(R.id.button_zDepth_5)
     protected void onClickZDepth5() {
-        mZDepthShadowLayout.changeZDepth(ZDepth.Depth5);
+        mZDepthShadowLayoutRect.changeZDepth(ZDepth.Depth5);
+        mZDepthShadowLayoutOval.changeZDepth(ZDepth.Depth5);
     }
 }
